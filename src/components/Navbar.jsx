c7f442e3a4d930a96f4e9103a7be122ee6d33a69
Bhavana Pabba <pabba.bhavana26@gmail.com>
2023-09-20 16:22:00 -0700

    github

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -102,7 +102,7 @@ const Navbar = () => {
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333333]'>
             <a
               className='flex justify-between items-center w-full text-gray-300'
-              href='https://pabbabhavana.github.io/myportfolio/'
+              href='https://github.com/PabbaBhavana'
             >
               Github <FaGithub size={30} />
             </a>
